3e8a771383a015ec82a37bd7176b555d6209eaad
Ilia-Luchnikov <luchnikovilya@gmail.com>
2026-01-05 15:43:39 +0400

    -documentation

---
+++ README.md
@@ -0,0 +1,21 @@
+## What is it?
+
+This is a package for large scale tensor-networks-based simulation of quantum annealing.
+It uses belief propagation based approximate inference (see https://arxiv.org/abs/2306.17837, https://arxiv.org/abs/2409.12240, https://arxiv.org/abs/2306.14887) as an engine. This implementation introduces a compilation step that classifies graph nodes by degree, groups the corresponding tensors into batched representations, groups the associated messages, enabling massively parallel belief propagation and related subroutines easelly deployable on a GPU.
+
+## How to install?
+
+1) Clone this repo;
+2) Run `pip install .` from the clonned repo under your python environment.
+
+To validate computation results some examples and tests require the exact quantum circuits simulator from https://github.com/LuchnikovI/qem.
+To install it do the following:
+
+1) Clone the repo https://github.com/LuchnikovI/qem;
+2) Install rust (see https://rust-lang.org/tools/install/);
+3) Install maturing by running `pip install maturin .`;
+4) Run `pip install .` from the clonned repo under your python environment.
+
+## How to use?
+
+This package exposes a single entry point, `run_qa`, which executes the full workflow. It accepts a single argument which is a Python dictionary that fully specifies the quantum annealing task. This dictionary serves as a configuration or DSL and can be directly deserialized from JSON or other formats. For a concrete example of the configuration, see `./examples/small_ibm_heavy_hex.py`.
--- examples/small_ibm_heavy_hex.py
+++ examples/small_ibm_heavy_hex.py
@@ -14,10 +14,10 @@ logging.basicConfig(
 # if something is wrong, the internal checker will throw an error explaining where is the problem
 
 config = {
-    "edges" : {       # this dict specifies interaction hamiltonian: its a map from edges to interaction amplitudes
-        (0, 1) : 1.,  # edges must not be duplicated (eg (1, 0) and (0, 1) in a single dict causes error)
-        (1, 2) : -1., # "edges" field is mandatory, "edges" field is flexible, it also can be a list or tuple of pairs
-        (2, 3) : 1.,  # i.e. one can deserialize it directly from json
+    "edges" : { # ------------------------> this dict specifies interaction hamiltonian: its a map from edges to interaction amplitudes
+        (0, 1) : 1., #                      edges must not be duplicated (eg (1, 0) and (0, 1) in a single dict causes error)
+        (1, 2) : -1., #                     "edges" field is mandatory, "edges" field is flexible, it also can be a list or tuple of pairs
+        (2, 3) : 1., #                      i.e. one can deserialize it directly from json
         (3, 4) : 1.,
         (0, 5) : -1.,
         (4, 6) : 1.,
@@ -38,9 +38,9 @@ config = {
         (18, 19) : -1.,
         (19, 20) : 1.,
     },
-    "nodes" : {       # this dict specifies local field acting on a node: its map from node to the amplitude of the field
-        0 : -1.,      # this field is optional, default is the empty dict {}
-        3 : -1.,      # if some node id is absent, the default value is taken from "default_field" (see below)
+    "nodes" : { # ------------------------> this dict specifies local field acting on a node: its map from node to the amplitude of the field
+        0 : -1., #                          this field is optional, default is the empty dict {}
+        3 : -1., #                          if some node id is absent, the default value is taken from "default_field" (see below)
         6 : -1.,
         7 : -1.,
         9 : -1.,
@@ -51,27 +51,49 @@ config = {
         17 : -1,
         20 : -1.,
     },
-    "default_field" : 1.,  # this is default field acting on the node, it is optional, default value is 0
-    "max_bond_dim" : 8,    # this is maximal bond dimension allowed during simulation, it is optional, default value is 4
-    "bp_eps" : 1e-10,      # this is the stopping discrepancy of BP algorithm, it is optional, default value is 1e-10
-    "pinv_eps" : 1e-10,    # this is a pseudo inverse threshold, it is optional, default value is 1e-10
-    "max_bp_iters_number" : 100,    # this is the maximal BP iterations allowed, if BP reaches this value, warning is shown and BP is terminated, it is optional, default value is 100
-    "measurement_threshold" : 0.95, # this is a confedence thresold which allows to skip measurement simulation and directly project state to the corresponding direction, it is optional, default value is 0.95
-    "seed" : 42,         # this is random seed, it is optional, default value is 42
-    "backend" : "numpy", # this is a backend used for simulation, it is optional, default value is "numpy"
-    "schedule" : {                   # this field specifies the annealing schedule, it is optional, to see default value see the corresponding warning
-        "total_time" : 10.,          # total "physical" time of the annealing dynamics, it is optional, default value is 10
-        "starting_mixing" : 1.,      # starting mixing, i.e. simulation starts from starting_mixing * H_mix + (1 - starting_mixing) * H_int hamiltonian, it is optional, default value is 1.
-        "actions" : [                # this is a list with actions (QA instructions), it is optional, to see the default value, see the corresponding warning message
+    "default_field" : 1., # --------------> this is default field acting on the node, it is optional, default value is 0
 
-            # the following instruction corresponds to annealing dynamics:
-            {"time" : 1., # the part of the "physical" time dedicated to this instruction
-             "steps_number" : 100,  # number of discrete time steps within this instruction
-             "final_mixing" : 0.0,  # the mixing after the instruction is finished, i.e. final hamiltonian is final_mixing * H_mix + (1 - final_mixing) * H_int hamiltonian
-             },
-            # instruction requesting to collect density matrices into a list
+    "max_bond_dim" : 8, # ----------------> this is maximal bond dimension allowed during simulation, it is optional, default value is 4
+
+    "bp_eps" : 1e-10, # ------------------> this is the stopping discrepancy of BP algorithm, it is optional, default value is 1e-10
+
+    "pinv_eps" : 1e-10, # ----------------> this is a pseudo inverse threshold, it is optional, default value is 1e-10
+
+    "max_bp_iters_number" : 100, # -------> this is the maximal BP iterations allowed, if BP reaches this value,
+                                 #          warning is shown and BP is terminated, it is optional, default value is 100
+
+    "measurement_threshold" : 0.95, # ----> this is a confedence thresold which allows to skip measurement simulation
+                                    #       and directly project state to the corresponding direction, it is optional, default value is 0.95
+
+    "seed" : 42, # -----------------------> this is the random seed, it is optional, default value is 42
+
+    "backend" : "numpy", # ---------------> this is a backend used for simulation, it is optional, default value is "numpy"
+                         #                  other backends available are ["cupy"]
+
+    "schedule" : { # ---------------------> this field specifies the annealing schedule, it is optional, to see default value see the corresponding warning
+
+        "total_time" : 10., # ------------> total "physical" time of the annealing dynamics, it is optional, default value is 10
+
+        "starting_mixing" : 1., # --------> starting mixing, i.e. simulation starts from
+                                #           starting_mixing * H_mix + (1 - starting_mixing) * H_int hamiltonian, it is optional, default value is 1.
+
+        "actions" : [ # ------------------> this is the list with actions (QA instructions), it is optional, to see the default value,
+                      #                     see the corresponding warning message
+
+            # QA instruction describing annealing dynamics:
+            {
+                "time" : 1., # -----------> the part of the "physical" time dedicated to this instruction
+
+                "steps_number" : 100, # --> number of discrete time steps within this instruction
+
+                "final_mixing" : 0.0, # --> the mixing after the instruction is finished, i.e. final hamiltonian is
+                                      #     final_mixing * H_mix + (1 - final_mixing) * H_int hamiltonian
+            },
+
+            # QA instruction requesting to collect density matrices into a list
             "get_density_matrices",
-            # instruction requesting to perform measurements of all nodes, results are collected as integer values into a list
+
+            # QA instruction requesting to perform measurements of all nodes, results are collected as integer values into a list
             "measure",
         ]
     },
