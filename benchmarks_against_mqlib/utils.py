ba51f7f707754b043d7cfda0d71c9b6c3b8f5e96
Ilia-Luchnikov <luchnikovilya@gmail.com>
2026-02-02 17:12:42 +0400

    benchmarking examples improved

--- benchmarks_against_mqlib/utils.py
+++ benchmarks_against_mqlib/utils.py
@@ -4,8 +4,12 @@ from statistics import mean, stdev
 from pathlib import Path
 from mqlib_wrap import run_heuristics, get_energy_function
 from bqa import run_qa
+from bqa.benchmarking import generate_qubo_on_2d_grid, generate_qubo_on_random_regular_graph
 
 script_dir = Path(__file__).resolve().parent
+
+GENERATORS = [generate_qubo_on_random_regular_graph, generate_qubo_on_2d_grid]
+GENERATORS_DICT = {gen.__name__ : gen for gen in GENERATORS}
 
 
 def project_bloch_vectors(bloch_vectors):
@@ -48,27 +52,61 @@ def get_some_graph_statistics(edges, nodes):
     }
 
 def replace_nodes_and_edges_with_statistics(config):
-    nodes = config["nodes"]
-    edges = config["edges"]
-    truncated_config = {k : v for k, v in config.items() if k not in {"nodes", "edges", "default_field"}}
-    return truncated_config | get_some_graph_statistics(edges, nodes)
+    truncated_config = {k : v for k, v in config.items() if k not in {"nodes", "edges", "default_field", "args", "kwargs"}}
+    return truncated_config | get_some_graph_statistics(config["edges"], config["nodes"])
 
 
-def run_benchmarks(config):
-    output_dir = script_dir / config["experiment_name"]
+def gen_config_slices(config):
+
+    def slice_config(coursor):
+        return {k : v[coursor[k][0]] if k in coursor else v for k, v in config.items()}
+
+    coursor = {k : [0, len(v)] for k, v in config.items() if isinstance(v, list)}
+    yield slice_config(coursor)
+    while True:
+        for key, (ptr, size) in reversed(coursor.items()):
+            if ptr < size - 1:
+                coursor[key][0] += 1
+                break
+            else:
+                coursor[key][0] = 0
+        if all(v[0] == 0 for v in coursor.values()):
+            break
+        yield slice_config(coursor)
+
+
+def _run_benchmarks(config_slice):
+    seed = int(config_slice["seed"])
+    gen_name = config_slice["generator_function_name"]
+    if gen_name in GENERATORS_DICT:
+        generator_func = GENERATORS_DICT[gen_name]
+    else:
+        raise ValueError(f"Unknown generator {gen_name}")
+    args = config_slice.get("args") or {}
+    kwargs = config_slice.get("kwargs") or {}
+    nodes, edges = generator_func(*args.values(), **kwargs, seed=seed)
+    config_slice["nodes"] = nodes
+    config_slice["edges"] = edges
+    output_dir = script_dir / config_slice["experiment_name"]
     output_dir.mkdir(parents=True, exist_ok=True)
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    bptn_result = run_qa(config)
-    mqlib_results = run_heuristics(config)
-    energy_function = get_energy_function(config)
+    bptn_result = run_qa(config_slice)
+    mqlib_results = run_heuristics(config_slice)
+    energy_function = get_energy_function(config_slice)
     bptn_configuration = extract_argmin_from_last_record(bptn_result)
     bptn_energy = energy_function(bptn_configuration)
     all_results = [(name, result) for name, result in mqlib_results.items()]
     all_results.sort(key = lambda pair: pair[1]["energy"])
     config_and_results = {
-        "config" : replace_nodes_and_edges_with_statistics(config),
+        "config" : replace_nodes_and_edges_with_statistics(config_slice),
         "bptn_result" : {"energy" : bptn_energy, "configuration" : bptn_configuration},
         "results" : all_results,
     }
     with open(output_dir / f"{timestamp}_result.json", "w") as file:
         json.dump(config_and_results, file)
+
+
+def run_benchmarks(config):
+    for config_slice in gen_config_slices(config):
+        _run_benchmarks(config_slice)
+
